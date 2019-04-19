40b109b55a54f31c9f6372e23efe1ed6c81470c6
Davi <idwgaming@hotmail.com>
2019-04-19 11:02:25 -0300

    fix bugs 18/04

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 echo "Make sure, you're executing this program in sudo."
 
 #Inicializa o docker builder // caso contrário, mostra mensagem de erro.
-#docker build -t sftp-explorer . || read -p "docker.io is not installed, you want to install it? [Y/N]: " ifstate
+docker build -t sftp-explorer . || read -p "docker.io is not installed, you want to install it? [Y/N]: " ifstate
 
 fileName="sftp.sh"
 
@@ -10,19 +10,17 @@ fileName="sftp.sh"
 if [ $ifstate=="Y" ];
 then
   apt-get install docker.io
-  #Inicializa a imagem no docker através do outro sh
-
-  touch $fileName
-  echo "echo \"Make sure you're executing in sudo.\"" >> $fileName
-  echo "docker run -ti sftp-explorer" >> $fileName
-  chmod 766 $fileName
-
-  sh start.sh
-  echo "Now, you can only execute 'start.sh' file."
-
-  #Remove esse arquivo
-  rm startup.sh
-
 else
   echo "Program installer canceled."
 fi
+
+#Inicializa a imagem no docker através do outro sh
+touch $fileName
+echo "echo \"Make sure you're executing in sudo.\"" >> $fileName
+echo "docker run -ti sftp-explorer" >> $fileName
+chmod 766 $fileName
+
+echo "Now, you can execute 'start.sh' file to start the app."
+
+#Remove esse arquivo
+rm startup.sh
